43db67e04d13e39591a74f42b70ba5c354c9e868
apg360 <53176930+apg360@users.noreply.github.com>
2020-10-18 18:12:08 +0200

    initial

---
+++ compile.sh
@@ -0,0 +1,37 @@
+#!/bin/zsh
+
+#Pre-requisite, must be installed in the system :
+# (*) Cmake
+# (*) Ninja
+# (*) LLVM + Clang
+# (*) Vulkan SDK from LunarG
+# (*) GLFW
+# (*) Command_Line_Tools_for_Xcode_12.dmg
+
+export PATH=~/MyProjects/clang+llvm-10.0.0-x86_64-apple-darwin/bin:~/MyProjects/Ninja1.10.1/ninja:$PATH
+export CC=clang
+export CXX=clang++
+export SDKROOT='/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk'
+export CPATH='/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk:/opt/local/include'
+export LIBRARY_PATH='/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/lib:/opt/local/lib'
+
+#Use cmake to perform OS operation
+cmake -E rm -rf build           #delete directory build
+cmake -E make_directory build   #create directory build
+
+#Use cmake to run the generator
+#cmake -E chdir build cmake -E time cmake ../   #run cmake to configure in directory 'build"
+cmake -E time cmake -G Ninja . -B build         #Use Ninja as generator on the CMakeLists in current directory, and generate under directory 'build'
+
+#Use cmake to run the build
+#ninja -j 4 -C build
+#ninja -j 4 install
+cmake -E time cmake --build build
+
+glslc shader.vert -o vert.spv
+glslc shader.frag -o frag.spv
+cmake -E copy vert.spv frag.spv build/
+
+echo -e "\n********* STARTING ********\n"
+build/./HelloTriangle
+echo -e "\n\n"
